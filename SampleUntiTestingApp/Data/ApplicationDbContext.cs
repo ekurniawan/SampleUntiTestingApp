da3175f3ff4f1d732c7a0b6f93799908a0610c78
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-12-28 09:06:55 +0700

    menambahkan appdbcontext

---
+++ SampleUntiTestingApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SampleUntiTestingApp.Models;
+
+namespace SampleUntiTestingApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<CreditCardApplication> CreditCardApplications { get; set; }
+    }
+}
