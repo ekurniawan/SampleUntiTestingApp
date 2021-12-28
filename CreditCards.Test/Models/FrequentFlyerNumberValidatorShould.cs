3c8dc4e7583a43661857d69f5e345299117dd2ea
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-12-28 09:57:18 +0700

    menambahkan xUnit project

---
+++ CreditCards.Test/Models/FrequentFlyerNumberValidatorShould.cs
@@ -0,0 +1,20 @@
+﻿using SampleUntiTestingApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CreditCards.Test.Models
+{
+    public class FrequentFlyerNumberValidatorShould
+    {
+        [Fact]
+        public void AcceptValidSchemes()
+        {
+            var sut = new FrequentFlyerNumberValidator();
+            Assert.True(sut.IsValid("123456-A"));
+        }
+    }
+}
