20f377a8cc59456b13770b5b50cae91d9e9c18b9
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-12-28 09:23:23 +0700

    menambahkan controller

---
+++ SampleUntiTestingApp/Controllers/ApplyController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SampleUntiTestingApp.Data;
+using SampleUntiTestingApp.Dtos;
+using SampleUntiTestingApp.Models;
+using System.Threading.Tasks;
+
+namespace SampleUntiTestingApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplyController : ControllerBase
+    {
+        private readonly ICreditCardApplicationRepository _applicationRepository;
+
+        public ApplyController(ICreditCardApplicationRepository applicationRepository)
+        {
+            _applicationRepository = applicationRepository;
+        }
+
+        public async Task<ActionResult> Add(NewCreditCardApplicationDetails applicationDetails)
+        {
+            var creditCardApplication = new CreditCardApplication
+            {
+                FirstName = applicationDetails.FirstName,
+                LastName = applicationDetails.LastName,
+                Age = applicationDetails.Age.Value,
+                GrossAnnualIncome=applicationDetails.GrossAnnualIncome.Value
+            };
+            await _applicationRepository.AddAsync(creditCardApplication);
+            return Ok(creditCardApplication);
+        }
+    }
+}
