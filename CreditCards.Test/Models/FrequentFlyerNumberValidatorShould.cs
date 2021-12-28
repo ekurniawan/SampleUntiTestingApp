e0c53a40f83c213df1cb543a7b7014c3c5095fc2
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-12-28 10:31:37 +0700

    menambahkan test case

--- CreditCards.Test/Models/FrequentFlyerNumberValidatorShould.cs
+++ CreditCards.Test/Models/FrequentFlyerNumberValidatorShould.cs
@@ -10,11 +10,40 @@ namespace CreditCards.Test.Models
 {
     public class FrequentFlyerNumberValidatorShould
     {
-        [Fact]
-        public void AcceptValidSchemes()
+        [Theory]
+        [InlineData("012345-A")]
+        [InlineData("012345 Q")]
+        [InlineData("012345-Y")]
+        public void AcceptValidSchemes(string number)
         {
             var sut = new FrequentFlyerNumberValidator();
-            Assert.True(sut.IsValid("123456-A"));
+            Assert.True(sut.IsValid(number));
+        }
+
+
+        [Theory]
+        [InlineData("012345-1")]
+        [InlineData("012345- ")]
+        [InlineData("012345- ")]
+        public void RejectInvalidSchemes(string number)
+        {
+            var sut = new FrequentFlyerNumberValidator();
+            Assert.False(sut.IsValid(number));
+        }
+
+        [Theory]
+        [InlineData("      -A")]
+        public void RejectEmptyMemberNumberDigits(string number)
+        {
+            var sut = new FrequentFlyerNumberValidator();
+            Assert.False(sut.IsValid(number));
+        }
+
+        [Fact]
+        public void ThrowExceptionWhenNullFrequentFlyerNumber()
+        {
+            var sut = new FrequentFlyerNumberValidator();
+            Assert.Throws<ArgumentNullException>(() => sut.IsValid(null));
         }
     }
 }
